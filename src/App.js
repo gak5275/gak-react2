96fb84b77350c298c9b181d46494643cae8cb2c5
gak5275 <gak5275@psu.edu>
2023-11-13 13:24:40 -0500

    added react project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import murray from './images/murray.png'
+import { useState } from 'react';
+import { sculptureList } from './data.js';
 
-function App() {
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < sculptureList.length - 1;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={murray} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <body>
+          <h1>Photoshop Projects from ART 168</h1>
+          <p>These are projects that I made for my Digital Media class using Adobe Photoshop. In this class, we learned how to use new tools in Photoshop each week and are tasked with creating "sketches" using those tools (basically just taking a pre-existing image and editing it). These are a few of mine that I thought turned out well.</p>
+          <button onClick={handleNextClick}>
+            Next
+          </button>
+
+          <h3>
+            ({index + 1} of {sculptureList.length})
+          </h3>
+          <button onClick={handleMoreClick}>
+            {showMore ? 'Hide' : 'Show'} details
+          </button>
+          {showMore && <p>{sculpture.description}</p>}
+          <br/>
+          <img
+              src={sculpture.url}
+              alt={sculpture.alt}
+          />
+        </body>
+      </>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+import './App.css';
+export const sculptureList = [{
+
+    description: 'For this sketch, I added scanlines to the image and gave the palm tree a glitch effect.',
+    url: 'https://gak5275.github.io/gak_repo2/Art%20168%20Projects/Sketch%205%20copy.jpg',
+    alt: 'Image #1'
+}, {
+
+    description: 'For this sketch, I added a corruption/static effect to the TV and tinted the image blue.',
+    url: 'https://gak5275.github.io/gak_repo2/Art%20168%20Projects/Sketch%206%20copy.jpg',
+    alt: 'Image #2'
+}, {
+
+    description: 'In this sketch, I burred the background of the image to put more emphasis on the Empire State Building.',
+    url: 'https://gak5275.github.io/gak_repo2/Art%20168%20Projects/Sketch%207%20copy.jpg',
+    alt: 'Image #3'
+}, {
+
+    description: 'In this sketch, I burred the background of the image to put more emphasis on the lightning strike.',
+    url: 'https://gak5275.github.io/gak_repo2/Art%20168%20Projects/Sketch%208%20copy.jpg',
+    alt: 'Image #4'
+}];
